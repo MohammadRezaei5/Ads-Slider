81457898f86a0a497cfb5b2940e1a2505f14aba2
mhmmd <mhmmd.rezaei4@gmail.com>
2023-08-18 15:51:51 +0330

    add project

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  // spaceBetween: 30,
+  grabCursor: true,
+  // loop: true,
+  // fade: "true",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  // mousewheel: true,
+  // keyboard: true,
+});
